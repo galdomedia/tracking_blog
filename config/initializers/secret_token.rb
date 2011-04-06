f507f65993ae809c08735f0b93dc2fb07e57d150
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-04-06 16:54:15 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackingBlog::Application.config.secret_token = 'e53d43745b887ff566ff5445491145c8d18ea63eefc69a443359248fc4ea2eecde233ee5e8d8240ec3b6cd89cf894af1ddf2d9be220cbc5a860b9842ac1444c0'
