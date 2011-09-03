228e83b3918d5044a03f274aeb2060e939f674c4
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-09-03 10:49:23 +0200

    coderay enabled

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 require 'coderay'
 module PostsHelper
   def highlight_source(text)
-    text.gsub(/\<code\s(lang=[\'"](.+?)[\'"])?\>(.+?)\<\/code\>/m) do
+    text.gsub(/\<code( lang=[\'"](.+?)[\'"])?\>(.+?)\<\/code\>/m) do
       CodeRay.scan($3, $2).div(:css => :class)
     end
   end
